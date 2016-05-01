8885c174d411ecb83c5cc70cf6f64b872e791bad
mdxprograms <mdx.programs@gmail.com>
2016-05-01 00:51:51 -0400

    updating var to let and url to a const

--- js/components/flights.js
+++ js/components/flights.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import Flight from './flight';
 
+const endpoint = '/js/data/flight_data.json';
+
 export default class Flights extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [],
-      url: '/js/data/flight_data.json'
+      data: []
     };
   }
 
   componentDidMount() {
-    var getFlights = function(url) {
+    let getFlights = function(url) {
       return fetch(url).then(response => response.json())
     };
-    getFlights(this.state.url).then((data) => {
+    getFlights(endpoint).then((data) => {
       this.setState({data});
     });
   }
 
   render() {
-    var flights = this.state.data.map((flight, i) =>
+    let flights = this.state.data.map((flight, i) =>
       <li key={i}><Flight raw={flight} /></li>
     );
     return <ul>{flights}</ul>;
